8842735136b858c36213f72aed198fd2ac1f454a
ivan solodov <ivansolobear@gmail.com>
2024-08-29 14:20:07 +0400

    chore: Add @tanstack/react-query and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 function App() {
   return (
     <div className="App">
-      <Dashboard />
+      <QueryClientProvider client={queryClient}>
+        <Dashboard />
+      </QueryClientProvider>
     </div>
   );
 }
